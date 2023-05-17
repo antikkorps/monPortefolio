169082fa3480260eb1566e8fccd8512e79a81dd6
Franck <franckvienot7@gmail.com>
2023-05-17 21:19:55 +0200

    added scroll tracking to navbar w/ scroll-timeline Polyfill

---
+++ src/components/scroll-tracker.js
@@ -0,0 +1,13 @@
+import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+
+const scrollTracker = document.querySelector('.scroll-tracker');
+scrollTracker.animate(
+  { transform: ['scaleX(0)', 'scaleX(1)'] },
+  {
+    duration: 10000, // Totally arbitrary!
+    fill: 'both',
+    timeline: new ScrollTimeline({
+      scrollOffsets: [new CSSUnitValue(0, 'px'), new CSSUnitValue(200, 'px')],
+    }),
+  }
+);
