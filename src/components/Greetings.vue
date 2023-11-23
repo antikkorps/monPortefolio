87102d27e6c48403dc17483858142eb47ef893a9
Franck <franckvienot7@gmail.com>
2023-11-23 01:07:18 +0100

    changed some colors and display in Navbar. Pass Vue component to script setup to test

--- src/components/Greetings.vue
+++ src/components/Greetings.vue
@@ -1,15 +1,14 @@
-<script lang="ts">
-export default {
-  data() {
-    return {
-      greeting: 'Hello World!',
-    };
-  },
-};
+<script setup lang="ts">
+const greeting = "Hello World!"
+
+const count = () => {
+  console.log("count")
+}
 </script>
 
 <template>
   <p class="greeting">{{ greeting }}</p>
+  <a @click="count" class="btn">count</a>
 </template>
 
 <style>
