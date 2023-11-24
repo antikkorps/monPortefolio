858fc7380f9f848ba3b1804b9156c4d31c771e9a
Franck <franckvienot7@gmail.com>
2023-11-24 12:25:07 +0100

    made some changes in Navbar and Hero

--- src/components/Greetings.vue
+++ src/components/Greetings.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
 const greeting = "Hello World!"
-
-const count = () => {
-  console.log("count")
-}
 </script>
 
 <template>
   <p class="greeting">{{ greeting }}</p>
-  <a @click="count" class="btn">count</a>
 </template>
 
 <style>
