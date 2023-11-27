5e53dfa0e8ecf397c26e540bccea56fd9231d082
Franck <franckvienot7@gmail.com>
2023-11-27 17:07:45 +0100

    added logic to display info on user choice in Quizz

--- src/components/QuizzFunctions.js
+++ src/components/QuizzFunctions.js
@@ -3,6 +3,10 @@
 const responses = ["Oui", "Oui", "Oui", "Oui"]
 const emojis = ["✅", "🚀", "😅", "😬"]
 
+const instructionsBeforeResult = document.querySelector(".instructions")
+const titleResult = document.querySelector(".results")
+const markResult = document.querySelector(".mark")
+const helpResult = document.querySelector(".message-text")
 document.querySelectorAll(".quizz-form").forEach((form) => {
   const submitButton = form.querySelector(".form_button")
   submitButton.addEventListener("click", (event) => handleSubmit(event, form))
@@ -16,15 +20,69 @@ function handleSubmit(event, form) {
   const radioButtons = form.querySelectorAll("input[type='radio']:checked")
 
   radioButtons.forEach((radioButton, index) => {
-    console.log("radio", radioButton)
     if (radioButton.value === responses[index]) {
       results.push(true)
-      console.log("results", results)
 
       const emoji = document.createElement("span")
       emoji.textContent = emojis[index]
       emoji.classList.add("emoji")
       radioButton.parentNode.appendChild(emoji)
+    } else {
+      results.push(false)
     }
   })
+  console.log("radio", radioButtons)
+  console.log("results", results)
+  showResults(results)
+}
+
+function showResults(results) {
+  const numberOfCorrectAnswers = results.filter((result) => result === true).length
+
+  console.log("Le nombre de réponse juste", numberOfCorrectAnswers)
+
+  switch (numberOfCorrectAnswers) {
+    case 4:
+      titleResult.textContent =
+        "La prochaine étape est bien de faire intervenir un développeur !"
+      markResult.textContent = "4/4"
+      helpResult.textContent =
+        "Et ça tombe bien je suis développeur ! Envoyez-moi un message"
+      markResult.style.display = "block"
+      helpResult.style.display = "block"
+      instructionsBeforeResult.style.display = "none"
+      break
+    case 3:
+      titleResult.textContent = "Très bonne nouvelle !"
+      markResult.textContent = "3/4"
+      helpResult.textContent = "Un dernier effort et votre projet aboutira !"
+      markResult.style.display = "block"
+      helpResult.style.display = "block"
+      instructionsBeforeResult.style.display = "none"
+      break
+    case 2:
+      titleResult.textContent = "C'est super la moitié du chemin est fait !"
+      markResult.textContent = "2/4"
+      helpResult.textContent = "Je peux vous aider à aller plus loin !"
+      markResult.style.display = "block"
+      helpResult.style.display = "block"
+      instructionsBeforeResult.style.display = "none"
+      break
+    case 1:
+      titleResult.textContent = "Nous avons une bonne base de travail !"
+      markResult.textContent = "1/4"
+      helpResult.textContent = "On s'y met quand ?"
+      markResult.style.display = "block"
+      helpResult.style.display = "block"
+      instructionsBeforeResult.style.display = "none"
+      break
+    default:
+      titleResult.textContent = "On est au début de votre projet !"
+      markResult.textContent = "0/4"
+      helpResult.textContent = "C'est une bonne nouvelle, rencontrons-nous !"
+      markResult.style.display = "block"
+      helpResult.style.display = "block"
+      instructionsBeforeResult.style.display = "none"
+      break
+  }
 }
